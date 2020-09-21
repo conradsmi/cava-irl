233fb15d80a4acd40040196b294bc645f4f6db51
conradsmi <conradsmith22@protonmail.com>
2020-09-21 14:22:30 -0400

    msg

--- cirlservice.c
+++ cirlservice.c
@@ -78,6 +78,7 @@ int main(int argc, char *argv[]) {
         // get name of client
         // TODO fix name
         name = calloc(0, INET_ADDRSTRLEN);
+        printf("%d", s->ai_family);
         if (getpeername(new_sock, s->ai_addr, &(s->ai_addrlen)) == 0) {
             inet_ntop(s->ai_family, getaddr(s->ai_addr), name, INET_ADDRSTRLEN);
             printf("Bound to client (%s); reading data and feeding it to GPIOs...\n", name);
